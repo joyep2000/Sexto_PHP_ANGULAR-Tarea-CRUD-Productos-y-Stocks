618a66c5cc991a43dd54b973ac56f3b565928a04
joyep2000 <joyep_2000@yahoo.com>
2024-01-14 23:42:31 -0500

    Tarea CRUD Producto y Stock

---
+++ front/src/app/Services/stock.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IStock } from '../Interfaces/istock';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class stockService {
+  private urlBase: string =
+    'http://localhost/Sexto_PHP_ANGULAR/Inventario/Controllers/Stock.Controller.php?op=';
+  constructor(private clientePhp: HttpClient) {}
+  todos(): Observable<IStock[]> {
+    return this.clientePhp.get<IStock[]>(this.urlBase + 'todos');
+  }
+  insertar(stock: IStock): Observable<any> {
+    var prov = new FormData();
+    prov.append('ProductoId', stock.ProductoId.toString());
+    prov.append('ProveedorId', stock.ProveedorId.toString());
+    prov.append('Cantidad', stock.Cantidad.toString());
+    prov.append('Precio_Venta', stock.Precio_Venta.toString());
+    return this.clientePhp.post(this.urlBase + 'insertar', prov);
+  }
+  eliminar(id: number): Observable<any> {
+    var prov = new FormData();
+    prov.append('stockId', id.toString());
+    return this.clientePhp.post(this.urlBase + 'eliminar', prov);
+  }
+  uno(id: number): Observable<IStock> {
+    var prov = new FormData();
+    prov.append('stockId', id.toString());
+    return this.clientePhp.post<IStock>(this.urlBase + 'uno', prov);
+  }
+  actualizar(stock: IStock, id: number): Observable<any> {
+    var prov = new FormData();
+    prov.append('stockId', id.toString());
+    prov.append('ProductoId', stock.ProductoId.toString());
+    prov.append('ProveedorId', stock.ProveedorId.toString());
+    prov.append('Cantidad', stock.Cantidad.toString());
+    prov.append('Precio_Venta', stock.Precio_Venta.toString());
+    return this.clientePhp.post(this.urlBase + 'actualizar', prov);
+  }
+}
---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,52 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './Views/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProductosComponent } from './Views/productos/productos.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+import { ProveedoresComponent } from './Views/proveedores/proveedores.component';
+import { StocksComponent } from './Views/stocks/stocks.component';
+import { NuevoProveedorComponent } from './Views/proveedores/nuevo-proveedor/nuevo-proveedor.component';
+import { NuevoProductoComponent } from './Views/productos/nuevo-producto/nuevo-producto.component';
+import { NuevoStockComponent } from './Views/stocks/nuevo-stock/nuevo-stock.component';
+
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  { path: 'productos', component: ProductosComponent },
+  {
+    path: 'nuevo-producto',
+    component: NuevoProductoComponent,
+  },
+  {
+    path: 'editar-producto/:id',
+    component: NuevoProductoComponent,
+  },
+  {    
+    path: 'proveedores',
+    component: ProveedoresComponent,
+  },
+  {
+    path: 'nuevo-proveedor',
+    component: NuevoProveedorComponent,
+  },
+  {
+    path: 'editar-proveedor/:id',
+    component: NuevoProveedorComponent,
+  },
+  {
+    path: 'stocks',
+    component: StocksComponent,
+  },
+  {
+    path: 'nuevo-stock',
+    component: NuevoStockComponent,
+  },
+  {
+    path: 'editar-stock/:id',
+    component: NuevoStockComponent,
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponentComponent },
+];
